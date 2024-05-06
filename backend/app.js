386e8dd9de152a2fa54266c54fe80439003005e6
Akashbabu1404 <akashmbabu@gmail.com>
2024-05-06 15:03:34 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,12 @@ const app = express();
 // Configure CORS
 
 
-app.use(cors({
-  origin: 'http://localhost:3000', // Set the origin to allow requests from any origin
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000', // Set the origin to allow requests from any origin
+//   credentials: true,
+// }));
+
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
